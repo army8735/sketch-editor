479f1f48fb52b86c8ab48b8a196a8af3ad3ff91f
Army <army8735@qq.com>
2023-06-28 10:35:58 +0800

    fix: 默认icon

--- demo/demo.js
+++ demo/demo.js
@@ -341,20 +341,25 @@ function genNodeTree(node, abHash) {
   let type = getNodeType(node);
   if (node instanceof editor.node.Geom || node instanceof editor.node.ShapeGroup) {
     const rect = node.rect;
-    const width = rect[2] - rect[0];
-    const height = rect[3] - rect[1];
-    let scale, x = 0, y = 0;
-    if (width >= height) {
-      scale = 14 / width;
-      y = (14 - height * scale) * 0.5;
+    let width = rect[2] - rect[0];
+    let height = rect[3] - rect[1];
+    if (!width || !height) {
+      type = `<b><svg width="14" height="14"><path d="M0,0L10,0L10,10L0,10L0,0ZM4,4L14,4L14,14L4,14,L4,4Z" fill="#D8D8D8" fill-rule="evenodd" stroke="#979797" stroke-width="1"></path></svg></b>`;
     }
     else {
-      scale = 14 / height;
-      x = (14 - width * scale) * 0.5;
+      let scale, x = 0, y = 0;
+      if(width >= height) {
+        scale = 14 / width;
+        y = (14 - height * scale) * 0.5;
+      }
+      else {
+        scale = 14 / height;
+        x = (14 - width * scale) * 0.5;
+      }
+      x -= rect[0] * scale;
+      y -= rect[1] * scale;
+      type = `<b style="transform:translate(${x}px, ${y}px) scale(${scale})">` + node.toSvg(scale) + '</b>';
     }
-    x -= rect[0] * scale;
-    y -= rect[1] * scale;
-    type = `<b style="transform:translate(${x}px, ${y}px) scale(${scale})">` + node.toSvg(scale) + '</b>';
   }
   const li = document.createElement('li');
   if (node.computedStyle.maskMode) {
