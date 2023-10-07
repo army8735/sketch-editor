7835a1d6f3f765cba0051925529f26d6f793559d
Army <army8735@qq.com>
2023-10-07 11:18:37 +0800

    chore: update

--- src/format/sketch.ts
+++ src/format/sketch.ts
@@ -1257,7 +1257,7 @@ async function loadPdf(blob: Blob): Promise<HTMLImageElement> {
   // @ts-ignore
   const pdfjsLib = window.pdfjsLib;
   pdfjsLib.GlobalWorkerOptions.workerSrc =
-    'https://gw.alipayobjects.com/os/lib/pdfjs-dist/3.9.179/build/pdf.worker.min.js';
+    'https://gw.alipayobjects.com/os/lib/pdfjs-dist/3.11.174/build/pdf.worker.min.js';
   const url = URL.createObjectURL(blob);
   const task = await pdfjsLib.getDocument(url).promise;
   const page = await task.getPage(1);
