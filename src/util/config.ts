af1c3dddda59b91970ed98615aa0b20597cf5f92
Army <army8735@qq.com>
2024-03-30 10:07:05 +0800

    chore: 8192

--- src/util/config.ts
+++ src/util/config.ts
@@ -2,14 +2,14 @@ export default {
   debug: false,
   offscreenCanvas: false,
   tile: false,
-  canvasSize: 2048,
-  MAX_TEXTURE_SIZE: 2048,
+  canvasSize: 8192,
+  MAX_TEXTURE_SIZE: 8192,
   MAX_TEXTURE_UNITS: 8,
   MAX_VARYING_VECTORS: 15,
   init(maxSize: number, maxUnits: number, maxVectors: number) {
     this.MAX_TEXTURE_SIZE = maxSize;
     this.MAX_TEXTURE_UNITS = maxUnits;
     this.MAX_VARYING_VECTORS = maxVectors;
-    this.canvasSize = Math.min(this.canvasSize, maxSize);
+    this.canvasSize = maxSize;
   },
 };
