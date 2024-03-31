e8127b47e85fae6900a6cf9e7b6a431c4ccc5b94
Army <army8735@qq.com>
2024-03-31 15:29:39 +0800

    chore:

--- test/group2/move-histroy/test.js
+++ test/group2/move-histroy/test.js
@@ -12,9 +12,10 @@ module.exports = {
       .moveTo('canvas', 30, 20)
       .mouseButtonUp(0)
       .execute(() => {
-        const style = window.root.getCurPage().children[0].style;
+        const node = window.root.getCurPage().children[0];
+        const style = node.style;
         const input = document.querySelector('input');
-        input.value = JSON.stringify([style.left, style.right, style.top, style.bottom]);
+        input.value = JSON.stringify([node.getBoundingClientRect(), style.left, style.right, style.top, style.bottom]);
       }, [], () => {
         browser
           .assert.value('input', '[{"v":10,"u":2},{"v":-110,"u":2},{"v":10,"u":2},{"v":-110,"u":2}]')
