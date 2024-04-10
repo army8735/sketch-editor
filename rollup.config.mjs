0e88be7b9cc047c0e3668eadaf4dcbda20bb0029
Asher <asherjia2012@gmail.com>
2024-04-10 15:22:35 +0800

    feat: 优化打包

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,58 +1,76 @@
-import terser from '@rollup/plugin-terser';
-import json from '@rollup/plugin-json';
-import typescript from '@rollup/plugin-typescript';
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import del from 'rollup-plugin-delete'
+import json from '@rollup/plugin-json'
+import terser from '@rollup/plugin-terser'
+import { defineConfig } from 'rollup'
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import glslify from 'rollup-plugin-glslify';
 
-export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        name: 'editor',
-        file: 'dist/index.js',
-        format: 'umd',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      nodeResolve({ preferBuiltins: false }),
-      commonjs(),
-      glslify(),
-      typescript(),
-      json(),
-    ],
-  },
-  {
-    input: 'src/index.ts',
-    output: {
-      name: 'editor',
-      file: 'dist/index.min.js',
-      format: 'umd',
-      sourcemap: true,
+const publicConfig = {
+    format: 'umd',
+    name: 'editor'
+}
+
+const config = defineConfig([
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: 'dist/index.js',
+                ...publicConfig
+            },
+            {
+                file: 'dist/index.min.js',
+                ...publicConfig,
+                plugins: [
+                    terser()
+                ]
+            }
+        ],
+        plugins: [
+          nodeResolve({ preferBuiltins: false }),
+          commonjs(),
+          glslify(),
+          typescript({
+            declaration: false,
+            target: "ES5"
+          }),
+          json(),
+        ]
     },
-    plugins: [
-      nodeResolve({ preferBuiltins: false }),
-      commonjs(),
-      glslify(),
-      typescript(),
-      terser({
-      }),
-      json(),
-    ],
-  },
-  {
-    input: 'src/index.ts', // 你的主入口文件
-    output: {
-      file: 'dist/index.esm.js', // 输出文件
-      format: 'esm', // ES模块格式
-      sourcemap: true,
+    {
+        input: 'src/index.ts',
+        output: {
+            file: 'dist/index.mjs',
+            format: 'esm'
+        },
+        plugins: [
+          glslify(),
+          typescript({
+            declaration: false
+          }),
+          json()
+        ]
     },
-    plugins:[
-      glslify(),
-      typescript(),
-      json()
-    ]
-  },
-];
+    // 归并 .d.ts 文件
+    {
+        input: 'types/index.d.ts',
+        output: {
+            file: 'dist/index.d.ts',
+            format: 'es'
+        },
+        plugins: [
+            // 将类型文件全部集中到一个文件中
+            dts(),
+            // 在构建完成后，删除 types 文件夹
+            del({
+                targets: 'types',
+                hook: 'buildEnd'
+            })
+        ]
+    }
+])
+
+export default config
