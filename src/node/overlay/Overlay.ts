4fba693f39d14a96fb5b4e1b6235c6c3dc635f59
Army <army8735@qq.com>
2024-04-19 09:44:08 +0800

    refactor:

--- src/node/overlay/Overlay.ts
+++ src/node/overlay/Overlay.ts
@@ -5,6 +5,7 @@ import Container from '../Container';
 import Node from '../Node';
 import SymbolMaster from '../SymbolMaster';
 import Text from '../Text';
+import { calPoint } from '../../math/matrix';
 
 class Overlay extends Container {
   artBoards: Container;
@@ -63,11 +64,12 @@ class Overlay extends Container {
     const artBoardList = this.artBoardList;
     for (let i = 0, len = artBoardList.length; i < len; i++) {
       const { artBoard, text } = artBoardList[i];
-      const rect = artBoard.getBoundingClientRect();
+      const rect = artBoard._rect || artBoard.rect;
+      const t = calPoint({ x: rect[0], y: rect[1]}, artBoard.matrixWorld);
       // 特殊更新，手动更新样式并计算，但不触发刷新，因为是在刷新过程中跟着画板当前位置计算的，避免再刷一次
       const res = text.updateStyleData({
-        translateX: rect.left,
-        translateY: rect.top - 32,
+        translateX: t.x,
+        translateY: t.y - 32,
       });
       if (res.length) {
         text.calMatrix(RefreshLevel.TRANSLATE);
