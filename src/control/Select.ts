564402af31c76fe1a68fddaf9225946be4f81068
Army <army8735@qq.com>
2024-05-14 10:43:54 +0800

    fix: hover旋转情况

--- src/control/Select.ts
+++ src/control/Select.ts
@@ -1,18 +1,19 @@
 import Node from '../node/Node';
 import Root from '../node/Root';
+import { identity, multiply, multiplyScale } from '../math/matrix';
 
 const html = `
   <span class="l" style="position:absolute;left:-4px;top:0;width:8px;height:100%;transform:scaleX(0.5);cursor:ew-resize;">
-    <b style="position:absolute;left:2px;top:0;width:0;height:100%;border-left:1px solid #F43;box-shadow:0 0 4px rgba(0,0,0,0.5);pointer-events:none;"></b>
+    <b style="position:absolute;left:4px;top:0;width:0;height:100%;border-left:1px solid #F43;box-shadow:0 0 4px rgba(0,0,0,0.5);pointer-events:none;"></b>
   </span>
   <span class="t" style="position:absolute;left:0;top:-4px;width:100%;height:8px;transform:scaleY(0.5);cursor:ns-resize;">
-    <b style="position:absolute;left:0;top:2px;width:100%;height:0;border-top:1px solid #F43;box-shadow:0 0 4px rgba(0,0,0,0.5);pointer-events:none;"></b>
+    <b style="position:absolute;left:0;top:4px;width:100%;height:0;border-top:1px solid #F43;box-shadow:0 0 4px rgba(0,0,0,0.5);pointer-events:none;"></b>
   </span>
   <span class="r" style="position:absolute;top:0;right:-4px;width:8px;height:100%;transform:scaleX(0.5);cursor:ew-resize;">
-    <b style="position:absolute;right:2px;top:0;width:0;height:100%;border-right:1px solid #F43;box-shadow:0 0 4px rgba(0,0,0,0.5);pointer-events:none;"></b>
+    <b style="position:absolute;right:4px;top:0;width:0;height:100%;border-right:1px solid #F43;box-shadow:0 0 4px rgba(0,0,0,0.5);pointer-events:none;"></b>
   </span>
   <span class="b" style="position:absolute;left:0;bottom:-4px;width:100%;height:8px;transform:scaleY(0.5);cursor:ns-resize;">
-    <b style="position:absolute;left:0;bottom:2px;width:100%;height:0;border-bottom:1px solid #F43;box-shadow:0 0 4px rgba(0,0,0,0.5);pointer-events:none;"></b>
+    <b style="position:absolute;left:0;bottom:4px;width:100%;height:0;border-bottom:1px solid #F43;box-shadow:0 0 4px rgba(0,0,0,0.5);pointer-events:none;"></b>
   </span>
   <span class="tl" style="position:absolute;left:0;top:0;width:14px;height:14px;border:1px solid #999;background:#FFF;box-shadow:0 0 4px rgba(0,0,0,0.5);transform:translate(-8px,-8px)scale(0.5);cursor:nwse-resize;"></span>
   <span class="tr" style="position:absolute;right:0;top:0;width:14px;height:14px;border:1px solid #999;background:#FFF;box-shadow:0 0 4px rgba(0,0,0,0.5);transform:translate(8px,-8px)scale(0.5);cursor:nesw-resize;"></span>
@@ -33,10 +34,12 @@ export default class Select {
     hover.className = 'hover';
     hover.style.display = 'none';
     hover.style.position = 'absolute';
-    hover.style.boxSizing = 'content-box';
+    hover.style.left = '0px';
+    hover.style.top = '0px';
+    hover.style.transformOrigin = '0 0';
+    hover.style.boxSizing = 'border-box';
     hover.style.border = '2px solid #F43';
     hover.style.boxShadow = '0 0 3px rgba(0, 0, 0, 0.5)';
-    hover.style.transform = 'translate(-2px, -2px)';
     dom.appendChild(hover);
     this.select = [];
   }
@@ -51,13 +54,18 @@ export default class Select {
   }
 
   updateHover(node: Node) {
-    const rect = node.getBoundingClientRect();
     const dpi = this.root.dpi;
     const hover = this.hover;
-    hover.style.left = rect.left / dpi + 'px';
-    hover.style.top = rect.top / dpi + 'px';
-    hover.style.width = (rect.right - rect.left) / dpi + 'px';
-    hover.style.height = (rect.bottom - rect.top) / dpi + 'px';
+    const rect = node._rect || node.rect;
+    let matrix = node.matrixWorld;
+    if (dpi !== 1) {
+      const t = identity();
+      multiplyScale(t, 1 / dpi);
+      matrix = multiply(t, matrix);
+    }
+    hover.style.width = (rect[2] - rect[0]) + 'px';
+    hover.style.height = (rect[3] - rect[1]) + 'px';
+    hover.style.transform = `matrix3d(${matrix.join(',')}`;
   }
 
   isHoverDom(dom: HTMLElement) {
@@ -70,6 +78,9 @@ export default class Select {
       const select = document.createElement('div');
       select.className = 'select';
       select.style.position = 'absolute';
+      select.style.left = '0px';
+      select.style.top = '0px';
+      select.style.transformOrigin = '0 0';
       select.style.boxSizing = 'content-box';
       select.innerHTML = html;
       this.dom.appendChild(select);
@@ -81,12 +92,17 @@ export default class Select {
   updateSelect(selected: Node[]) {
     const dpi = this.root.dpi;
     selected.forEach((item, i) => {
-      const rect = item.getBoundingClientRect();
       const select = this.select[i];
-      select.style.left = rect.left / dpi + 'px';
-      select.style.top = rect.top / dpi + 'px';
-      select.style.width = (rect.right - rect.left) / dpi + 'px';
-      select.style.height = (rect.bottom - rect.top) / dpi + 'px';
+      const rect = item._rect || item.rect;
+      let matrix = item.matrixWorld;
+      if (dpi !== 1) {
+        const t = identity();
+        multiplyScale(t, 1 / dpi);
+        matrix = multiply(t, matrix);
+      }
+      select.style.width = (rect[2] - rect[0]) + 'px';
+      select.style.height = (rect[3] - rect[1]) + 'px';
+      select.style.transform = `matrix3d(${matrix.join(',')}`;
     });
   }
 
