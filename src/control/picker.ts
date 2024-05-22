8e7fc7f9fe33eb6bba800c72c8c8dbd87cca7751
Army <army8735@qq.com>
2024-05-22 16:39:51 +0800

    fix:

--- src/control/picker.ts
+++ src/control/picker.ts
@@ -1,24 +1,12 @@
 import Picker from 'vanilla-picker';
 import { color2rgbaInt, color2rgbaStr } from '../style/css';
 
-const div = document.createElement('div');
-div.style.position = 'absolute';
-div.style.left = '0px';
-div.style.top = '0px';
-div.style.zIndex = '9999';
-div.style.display = 'none';
-div.style.padding = '10px';
-div.style.backgroundColor = '#FFF';
-div.style.border = '1px solid #CCC';
-div.style.borderRadius = '5px';
-div.style.filter = 'drop-shadow(0 0 5px rgba(0, 0, 0, 0.2))';
-div.style.transform = 'translateX(-50%)';
-div.innerHTML = `
+let div: HTMLElement;
+const html = `
 <span style="position:absolute;left:50%;top:0;border:10px solid transparent;border-bottom-color:#CCC;transform:translate(-50%,-100%)">
   <b style="position:absolute;left:0;top:0;border:10px solid transparent;border-bottom-color:#FFF;transform:translate(-10px,-9px)"></b>
 </span>
 `;
-document.body.appendChild(div);
 
 let picker: Picker;
 
@@ -32,6 +20,22 @@ export default {
     }
     callback = cb;
     const rect = node.getBoundingClientRect();
+    if (!div) {
+      div = document.createElement('div');
+      div.style.position = 'absolute';
+      div.style.left = '0px';
+      div.style.top = '0px';
+      div.style.zIndex = '9999';
+      div.style.display = 'none';
+      div.style.padding = '10px';
+      div.style.backgroundColor = '#FFF';
+      div.style.border = '1px solid #CCC';
+      div.style.borderRadius = '5px';
+      div.style.filter = 'drop-shadow(0 0 5px rgba(0, 0, 0, 0.2))';
+      div.style.transform = 'translateX(-50%)';
+      div.innerHTML = html;
+      document.body.appendChild(div);
+    }
     div.style.left = rect.left + (rect.right - rect.left) * 0.5 + 'px';
     div.style.top = rect.bottom + 10 + 'px';
     div.style.display = 'block';
