cddbb34ced34f5f2a21381c6cb751fd134615170
Army <army8735@qq.com>
2024-05-30 17:24:26 +0800

    test:

--- test/edit.js
+++ test/edit.js
@@ -2,6 +2,45 @@ let canvas = document.querySelector('canvas');
 let input = document.querySelector('#base64');
 sketchEditor.ca.preserveDrawingBuffer = true;
 
+let count = 0;
+let root;
+let listener;
+
+function toDataURL() {
+  input.value = (count++) + ',' + canvas.toDataURL();
+}
+
+function toStyle() {
+  let node = listener.selected[0];
+  if (!node) {
+    node = root.getCurPage().children[0];
+    if (node instanceof sketchEditor.node.Container) {
+      node = node.children[0];
+    }
+  }
+  const style = node.style;
+  const computedStyle = node.computedStyle;
+  input.value = JSON.stringify([
+    count++,
+    style.left,
+    style.right,
+    style.top,
+    style.bottom,
+    style.width,
+    style.height,
+    style.translateX,
+    style.translateY,
+    computedStyle.left,
+    computedStyle.right,
+    computedStyle.top,
+    computedStyle.bottom,
+    computedStyle.width,
+    computedStyle.height,
+    computedStyle.translateX,
+    computedStyle.translateY,
+  ]);
+}
+
 fetch('./sketch.sketch')
   .then((res) => {
     return res.arrayBuffer();
@@ -16,46 +55,18 @@ fetch('./sketch.sketch')
         canvas.width = 500 * dpi;
         canvas.height = 500 * dpi;
         $canvasC.appendChild(canvas);
-        const root = window.root = sketchEditor.parse(json, canvas, dpi);
-        const listener = sketchEditor.control.initCanvasControl(root, $canvasC);
+        root = window.root = sketchEditor.parse(json, canvas, dpi);
+        listener = sketchEditor.control.initCanvasControl(root, $canvasC);
         sketchEditor.control.initTreeList(root, document.querySelector('#tree'), listener);
         sketchEditor.control.initPanel(root, document.querySelector('#side'), listener);
-        let count = 0;
         $canvasC.addEventListener('mousedown', (e) => {
           if (e.button === 1) {
             e.preventDefault();
-            input.value = (count++) + ',' + canvas.toDataURL();
+            toDataURL();
           }
           else if (e.button === 2) {
             e.preventDefault();
-            let node = listener.selected[0];
-            if (!node) {
-              node = root.getCurPage().children[0];
-              if (node instanceof sketchEditor.node.Container) {
-                node = node.children[0];
-              }
-            }
-            const style = node.style;
-            const computedStyle = node.computedStyle;
-            input.value = JSON.stringify([
-              count++,
-              style.left,
-              style.right,
-              style.top,
-              style.bottom,
-              style.width,
-              style.height,
-              style.translateX,
-              style.translateY,
-              computedStyle.left,
-              computedStyle.right,
-              computedStyle.top,
-              computedStyle.bottom,
-              computedStyle.width,
-              computedStyle.height,
-              computedStyle.translateX,
-              computedStyle.translateY,
-            ]);
+            toStyle();
           }
         });
       });
