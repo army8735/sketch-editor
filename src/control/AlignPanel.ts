4ac7f2a3c9e650de9705c7b951a3e160ff8ff492
Army <army8735@qq.com>
2024-07-16 11:01:32 +0800

    feat: align-panel

--- src/control/AlignPanel.ts
+++ src/control/AlignPanel.ts
@@ -4,6 +4,7 @@ import Listener from './Listener';
 import Node from '../node/Node';
 import Group from '../node/Group';
 import ArtBoard from '../node/ArtBoard';
+import MoveCommand from '../history/MoveCommand';
 
 const html = `
   <h4 class="panel-title">对齐</h4>
@@ -36,7 +37,53 @@ class AlignPanel extends Panel {
           return;
         }
       }
-      this.nodes.forEach(item => {});
+      const classList = target.classList;
+      const nodes: Node[] = [];
+      const dxs: number[] = [];
+      const dys: number[] = [];
+      this.nodes.forEach(node => {
+        let dx = 0;
+        let dy = 0;
+        const parent = node.parent!;
+        const { width: pw, height: ph } = parent;
+        const computedStyle = node.computedStyle;
+        if (classList.contains('left')) {
+          dx = -computedStyle.left - computedStyle.translateX;
+        }
+        else if (classList.contains('center')) {
+          dx = pw * 0.5 - computedStyle.left - computedStyle.translateX - computedStyle.width * 0.5;
+        }
+        else if (classList.contains('right')) {
+          dx = pw - computedStyle.left - computedStyle.translateX - computedStyle.width;
+        }
+        else if (classList.contains('top')) {
+          dy = -computedStyle.top - computedStyle.translateY;
+        }
+        else if (classList.contains('middle')) {
+          dy = ph * 0.5 - computedStyle.top - computedStyle.translateY - computedStyle.height * 0.5;
+        }
+        else if (classList.contains('bottom')) {
+          dy = ph - computedStyle.top - computedStyle.translateY - computedStyle.height;
+        }
+        if (dx || dy) {
+          const style = node.getStyle();
+          node.updateStyle({
+            translateX: node.computedStyle.translateX + dx,
+            translateY: node.computedStyle.translateY + dy,
+          });
+          // 还原最初的translate/TRBL值
+          node.endPosChange(style, dx, dy);
+          node.checkPosSizeUpward();
+          nodes.push(node);
+          dxs.push(dx);
+          dys.push(dy);
+        }
+      });
+      if (nodes.length) {
+        listener.history.addCommand(new MoveCommand(nodes.slice(0), dxs, dys));
+        listener.select.updateSelect(nodes);
+        listener.emit(Listener.MOVE_NODE, nodes.slice(0));
+      }
     });
 
     listener.on(Listener.SELECT_NODE, (nodes: Node[]) => {
@@ -50,7 +97,7 @@ class AlignPanel extends Panel {
     nodes.forEach((node: Node) => {
       const p = node.parent;
       if (p && (p instanceof Group || p instanceof ArtBoard)) {
-        ns.push(p);
+        ns.push(node);
       }
     });
     this.nodes = ns;
