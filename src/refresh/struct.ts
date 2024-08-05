0a03a993eed387cb28deafdfecb7283c063a3b44
Army <army8735@qq.com>
2024-08-05 18:50:01 +0800

    fix:

--- src/refresh/struct.ts
+++ src/refresh/struct.ts
@@ -934,7 +934,7 @@ function renderWebglNoTile(
             target.bbox,
             scale,
           );
-          const w = H / root.dpi;
+          const w = W / root.dpi;
           const h = H / root.dpi;
           // 包装画布bg为TextureCache，默认画布不会超过尺寸限制，即便超过手动设置的，也不会超过系统
           const wrap = TextureCache.getEmptyInstance(gl, new Float64Array([0, 0, w, h]));
