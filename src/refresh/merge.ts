acd650e534d3414d73ee9914d79fde8fd6d5c232
Army <army8735@qq.com>
2024-08-30 10:17:49 +0800

    fix:

--- src/refresh/merge.ts
+++ src/refresh/merge.ts
@@ -347,7 +347,9 @@ function genBboxTotal(
       const parent = node2.parent!;
       const m = multiply(parent.tempMatrix, node2.matrix);
       assignMatrix(node2.tempMatrix, m);
-      const b = target?.bbox || node2._filterBbox2 || node2.filterBbox2;
+      // 合并不能用textureCache，因为如果有shadow的话bbox不正确
+      const b = (target && target !== node2.textureCache[scaleIndex]) ?
+        target.bbox : (node2._filterBbox2 || node2.filterBbox2);
       // 防止空
       if (b[2] - b[0] && b[3] - b[1]) {
         mergeBbox(res, b, m);
@@ -1983,7 +1985,9 @@ function genShadow(
     }
     // 删除fbo恢复
     res.release();
-    releaseFrameBuffer(gl, frameBuffer!, W, H);
+    if (frameBuffer) {
+      releaseFrameBuffer(gl, frameBuffer!, W, H);
+    }
   }
   // 将原本的图层绘到最上方
   let frameBuffer: WebGLFramebuffer | undefined;
@@ -2029,7 +2033,9 @@ function genShadow(
       }
     }
   }
-  releaseFrameBuffer(gl, frameBuffer!, W, H);
+  if (frameBuffer) {
+    releaseFrameBuffer(gl, frameBuffer!, W, H);
+  }
   return res2;
 }
 
