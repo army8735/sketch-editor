b0fc9cc95f657df4a70b4343098a37524213c5c1
Army <army8735@qq.com>
2024-09-05 17:35:32 +0800

    chore

--- test/group2/history-remove-index/test.js
+++ test/group2/history-remove-index/test.js
@@ -32,7 +32,7 @@ module.exports = {
       .click('#button4')
       .assert.value('#base64', '[[1,"1"],[1,"2"],[1,"3"]]')
       .assert.elementPresent('#tree span.name[title="1"]')
-      .keys(browser.Keys.EASCAPE)
+      .keys(browser.Keys.ESCAPE)
       .keys(browser.Keys.NULL)
       .pause(20)
 
@@ -42,8 +42,6 @@ module.exports = {
       .keys(browser.Keys.DELETE)
       .keys(browser.Keys.NULL)
       .pause(20)
-      .click('#button1')
-      .assert.value('#base64', '')
       .moveToElement('canvas', 300, 300)
       .mouseButtonDown(0)
       .moveToElement('canvas', 20, 20)
@@ -63,7 +61,7 @@ module.exports = {
       .click('#button4')
       .assert.value('#base64', '[[3,"1"],[3,"2"],[3,"3"]]')
       .assert.elementPresent('#tree span.name[title="2"]')
-      .keys(browser.Keys.EASCAPE)
+      .keys(browser.Keys.ESCAPE)
       .keys(browser.Keys.NULL)
       .pause(20)
 
