e4a42a207c5a14af927b939a1605f215346b29bb
Army <army8735@qq.com>
2024-09-11 16:32:06 +0800

    test:

--- test/group2/text-cursor-select-update/test.js
+++ test/group2/text-cursor-select-update/test.js
@@ -60,7 +60,7 @@ module.exports = {
       .click('#button3')
       .assert.value('#base64', '[3,[{"location":0,"length":1,"fontFamily":"Helvetica","fontSize":72,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[0,0,0,1]},{"location":1,"length":1,"fontFamily":"Helvetica","fontSize":72,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[255,66,66,1]},{"location":2,"length":2,"fontFamily":"Helvetica","fontSize":72,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[0,0,0,1]},{"location":4,"length":1,"fontFamily":"Helvetica","fontSize":85,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[0,0,0,1]},{"location":5,"length":2,"fontFamily":"Helvetica","fontSize":48,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[0,0,0,1]},{"location":7,"length":2,"fontFamily":"Helvetica","fontSize":72,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[0,0,0,1]}]]')
 
-      .moveToElement('canvas', 30, 180)
+      .moveToElement('canvas', 30, 210)
       .doubleClick()
       .click('#side .text-panel .al .center')
       .click('#button3')
