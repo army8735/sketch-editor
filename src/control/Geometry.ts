2e6e50f12ddcfc202bff79308075ff7a44465b32
Army <army8735@qq.com>
2025-03-03 11:29:19 +0800

    fix:

--- src/control/Geometry.ts
+++ src/control/Geometry.ts
@@ -237,7 +237,8 @@ export default class Geometry {
                 };
               }
             });
-            listener.emit(Listener.POINT_NODE, this.nodes.slice(0), data);
+            listener.history.addCommand(new PointCommand(this.nodes.slice(0), data), true);
+            listener.emit(Listener.POINT_NODE, [node], [[mid]]);
             this.emitSelectPoint();
             pathIdx = -1;
           }
