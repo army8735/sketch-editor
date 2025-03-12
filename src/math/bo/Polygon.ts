3c9d0723707a52c23ef99b7f44b56f2f73ad4d2b
Army <army8735@qq.com>
2025-03-12 11:21:19 +0800

    fix: 布尔切割是单调

--- src/math/bo/Polygon.ts
+++ src/math/bo/Polygon.ts
@@ -66,25 +66,25 @@ class Polygon {
             continue;
           }
           const cPoint = new Point(curr[0], curr[1]);
-          const t = bezier.getBezierMonotonicityT2([startPoint, cPoint, endPoint], true) || [];
-          const t2 = bezier.getBezierMonotonicityT2([startPoint, cPoint, endPoint], false);
-          const t3 = bezier.getBezierMonotonicityT([startPoint, cPoint, endPoint], true);
-          const t4 = bezier.getBezierMonotonicityT([startPoint, cPoint, endPoint], false);
+          const t = bezier.getBezierMonotonicityT([startPoint, cPoint, endPoint], true) || [];
+          const t2 = bezier.getBezierMonotonicityT([startPoint, cPoint, endPoint], false);
+          // const t3 = bezier.getBezierMonotonicityT2([startPoint, cPoint, endPoint], true);
+          // const t4 = bezier.getBezierMonotonicityT2([startPoint, cPoint, endPoint], false);
           t2?.forEach(i => {
             if (!t.includes(i)) {
               t.push(i);
             }
           });
-          t3?.forEach(i => {
-            if (!t.includes(i)) {
-              t.push(i);
-            }
-          });
-          t4?.forEach(i => {
-            if (!t.includes(i)) {
-              t.push(i);
-            }
-          });
+          // t3?.forEach(i => {
+          //   if (!t.includes(i)) {
+          //     t.push(i);
+          //   }
+          // });
+          // t4?.forEach(i => {
+          //   if (!t.includes(i)) {
+          //     t.push(i);
+          //   }
+          // });
           if (t.length) {
             const points: [number, number][] = [
               [startPoint.x, startPoint.y],
@@ -161,25 +161,25 @@ class Polygon {
           }
           const cPoint1 = new Point(curr[0], curr[1]),
             cPoint2 = new Point(curr[2], curr[3]);
-          const t = bezier.getBezierMonotonicityT2([startPoint, cPoint1, cPoint2, endPoint], true) || [];
-          const t2 = bezier.getBezierMonotonicityT2([startPoint, cPoint1, cPoint2, endPoint], false);
-          const t3 = bezier.getBezierMonotonicityT([startPoint, cPoint1, cPoint2, endPoint], true);
-          const t4 = bezier.getBezierMonotonicityT([startPoint, cPoint1, cPoint2, endPoint], false);
+          const t = bezier.getBezierMonotonicityT([startPoint, cPoint1, cPoint2, endPoint], true) || [];
+          const t2 = bezier.getBezierMonotonicityT([startPoint, cPoint1, cPoint2, endPoint], false);
+          // const t3 = bezier.getBezierMonotonicityT2([startPoint, cPoint1, cPoint2, endPoint], true);
+          // const t4 = bezier.getBezierMonotonicityT2([startPoint, cPoint1, cPoint2, endPoint], false);
           t2?.forEach(i => {
             if (!t.includes(i)) {
               t.push(i);
             }
           });
-          t3?.forEach(i => {
-            if (!t.includes(i)) {
-              t.push(i);
-            }
-          });
-          t4?.forEach(i => {
-            if (!t.includes(i)) {
-              t.push(i);
-            }
-          });
+          // t3?.forEach(i => {
+          //   if (!t.includes(i)) {
+          //     t.push(i);
+          //   }
+          // });
+          // t4?.forEach(i => {
+          //   if (!t.includes(i)) {
+          //     t.push(i);
+          //   }
+          // });
           if (t.length) {
             const points: Array<[number, number]> = [
               [startPoint.x, startPoint.y],
