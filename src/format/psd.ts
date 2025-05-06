f17a75dd45ed4f7a41c87bd042b05393e2c7cad4
Army <army8735@qq.com>
2025-05-06 13:28:41 +0800

    fix:

--- src/format/psd.ts
+++ src/format/psd.ts
@@ -84,7 +84,9 @@ export async function openAndConvertPsdBuffer(arrayBuffer: ArrayBuffer) {
             if (!child.clipping) {
               const d = i - j;
               const m = children[children.length - d];
-              m?.props?.style!.maskMode = 'alpha-with';
+              if (m) {
+                m.props.style!.maskMode = 'alpha-with';
+              }
               break;
             }
           }
@@ -242,7 +244,9 @@ async function convertItem(layer: Layer, w: number, h: number) {
               if (!child.clipping) {
                 const d = i - j;
                 const m = children[children.length - d];
-                m?.props?.style!.maskMode = 'alpha-with';
+                if (m) {
+                  m.props.style!.maskMode = 'alpha-with';
+                }
                 break;
               }
             }
