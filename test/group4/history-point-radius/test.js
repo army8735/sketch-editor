769869f0ff24bdb202ebe74abb1106180c4275dc
Army <army8735@qq.com>
2025-05-20 20:38:03 +0800

    test:

--- test/group4/history-point-radius/test.js
+++ test/group4/history-point-radius/test.js
@@ -15,12 +15,16 @@ module.exports = {
       // .assert.domPropertyEquals('#side .point-panel .num input[type="range"]', 'disabled', 'true')
       // .assert.domPropertyEquals('#side .point-panel .num input[type="number"]', 'disabled', 'true')
 
-      .click('#main .geometry .vt[title="0"]')
+      .moveToElement('canvas', 30, 30)
+      .doubleClick()
+      .click('#main .geometry .vt[title="0"]', 0, 0)
+      .assert.value('#side .point-panel .num input[type="number"]', 10)
+      .assert.value('#side .point-panel .num input[type="range"]', 10)
       // .assert.domPropertyEquals('#side .point-panel .num input[type="range"]', 'disabled', 'false')
       // .assert.domPropertyEquals('#side .point-panel .num input[type="number"]', 'disabled', 'false')
       .updateValue('#side .point-panel .num input[type="number"]', ['0', browser.Keys.ENTER])
       .click('#button6')
-      .assert.value('#base64', '[1,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":0.888888888888889,"y":0,"cornerRadius":20,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0.888888888888889,"fy":0,"tx":0.888888888888889,"ty":0,"absX":150,"absY":0,"absTx":150,"absTy":0,"absFx":150,"absFy":0},{"x":0.888888888888889,"y":0.888888888888889,"cornerRadius":0,"curveMode":2,"hasCurveFrom":true,"hasCurveTo":true,"fx":0.666666666666667,"fy":1.11111111111111,"tx":1.11111111111111,"ty":0.666666666666667,"absX":150,"absY":100,"absTx":187.49999999999983,"absTy":75.00000000000003,"absFx":112.50000000000006,"absFy":124.99999999999989},{"x":0,"y":0.888888888888889,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0.888888888888889,"tx":0,"ty":0.888888888888889,"absX":0,"absY":100,"absTx":0,"absTy":100,"absFx":0,"absFy":100}]]')
+      .assert.value('#base64', '[1,[{"x":0,"y":0,"cornerRadius":10,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":0.888888888888889,"y":0,"cornerRadius":20,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0.888888888888889,"fy":0,"tx":0.888888888888889,"ty":0,"absX":150,"absY":0,"absTx":150,"absTy":0,"absFx":150,"absFy":0},{"x":0.888888888888889,"y":0.888888888888889,"cornerRadius":0,"curveMode":2,"hasCurveFrom":true,"hasCurveTo":true,"fx":0.666666666666667,"fy":1.11111111111111,"tx":1.11111111111111,"ty":0.666666666666667,"absX":150,"absY":100,"absTx":187.49999999999983,"absTy":75.00000000000003,"absFx":112.50000000000006,"absFy":124.99999999999989},{"x":0,"y":0.888888888888889,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0.888888888888889,"tx":0,"ty":0.888888888888889,"absX":0,"absY":100,"absTx":0,"absTy":100,"absFx":0,"absFy":100}]]')
 
       .keys(browser.Keys.META)
       .keys('z')
