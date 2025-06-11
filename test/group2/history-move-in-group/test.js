f21e3a32297f51b123cb1248433f774a12a7ea5b
Army <army8735@qq.com>
2025-06-11 16:03:37 +0800

    chore:

--- test/group2/history-move-in-group/test.js
+++ test/group2/history-move-in-group/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .pause(100)
+      .pause(200)
       .keys(browser.Keys.META)
       .moveToElement('canvas', 120, 120)
       .mouseButtonDown(0)
