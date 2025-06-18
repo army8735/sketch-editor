57f8c34d9efba8a81b05122ffe16f8bff9ffee7b
Army <army8735@qq.com>
2025-06-18 16:56:21 +0800

    test:

--- test/group4/copy-edit-polyline/test.js
+++ test/group4/copy-edit-polyline/test.js
@@ -18,7 +18,7 @@ module.exports = {
       .keys(browser.Keys.ESCAPE)
       .keys(browser.Keys.NULL)
       .click('#button6')
-      .assert.value('#base64', '[0,]')
+      .assert.value('#base64', '[0,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":1,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":0,"tx":1,"ty":0,"absX":100,"absY":0,"absTx":100,"absTy":0,"absFx":100,"absFy":0},{"x":1,"y":1,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":1,"tx":1,"ty":1,"absX":100,"absY":100,"absTx":100,"absTy":100,"absFx":100,"absFy":100},{"x":0,"y":1,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":1,"tx":0,"ty":1,"absX":0,"absY":100,"absTx":0,"absTy":100,"absFx":0,"absFy":100},{"x":0.1,"y":0.4,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0.1,"fy":0.4,"tx":0.1,"ty":0.4,"absX":10,"absY":40,"absTx":10,"absTy":40,"absFx":10,"absFy":40}]]')
 
       .moveToElement('canvas', 50, 50)
       .mouseButtonDown(0)
@@ -38,7 +38,7 @@ module.exports = {
       // 老的不再选择
       .assert.not.cssClassPresent('dl[uuid="EEB43720-A2E0-444C-9E29-37489788E994"] dt', 'active')
       .click('#button6')
-      .assert.value('#base64', '[1,]')
+      .assert.value('#base64', '[1,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":1,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":0,"tx":1,"ty":0,"absX":100,"absY":0,"absTx":100,"absTy":0,"absFx":100,"absFy":0},{"x":1,"y":1,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":1,"tx":1,"ty":1,"absX":100,"absY":100,"absTx":100,"absTy":100,"absFx":100,"absFy":100},{"x":0,"y":1,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":1,"tx":0,"ty":1,"absX":0,"absY":100,"absTx":0,"absTy":100,"absFx":0,"absFy":100},{"x":0.1,"y":0.4,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0.1,"fy":0.4,"tx":0.1,"ty":0.4,"absX":10,"absY":40,"absTx":10,"absTy":40,"absFx":10,"absFy":40}]]')
 
       .end();
   }
