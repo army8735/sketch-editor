719add332fba0e57b33741f83fb22d4f716a43a4
Army <army8735@qq.com>
2025-07-01 14:00:54 +0800

    test:

--- test/group3/text-panel-color/test.js
+++ test/group3/text-panel-color/test.js
@@ -24,7 +24,7 @@ module.exports = {
       .keys(browser.Keys.ESCAPE)
       .keys(browser.Keys.NULL)
       .click('#button3')
-      .assert.value('#base64', '')
+      .assert.value('#base64', '[1,[{"location":0,"length":3,"fontFamily":"Helvetica","fontSize":60,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[78,78,78,1]},{"location":3,"length":1,"fontFamily":"Helvetica","fontSize":60,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[255,0,0,1]},{"location":4,"length":2,"fontFamily":"Helvetica","fontSize":60,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[78,78,78,1]}]]')
 
       .moveToElement('canvas', 100, 20)
       .doubleClick()
@@ -36,7 +36,7 @@ module.exports = {
       .keys(browser.Keys.ESCAPE)
       .keys(browser.Keys.NULL)
       .click('#button3')
-      .assert.value('#base64', '')
+      .assert.value('#base64', '[2,[{"location":0,"length":3,"fontFamily":"Helvetica","fontSize":60,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[78,78,78,1]},{"location":3,"length":1,"fontFamily":"Helvetica","fontSize":60,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[0,0,255,1]},{"location":4,"length":2,"fontFamily":"Helvetica","fontSize":60,"fontWeight":400,"fontStyle":"normal","letterSpacing":0,"textAlign":0,"textDecoration":[],"lineHeight":0,"paragraphSpacing":0,"color":[78,78,78,1]}]]')
 
       .end();
   }
