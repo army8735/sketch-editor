771a143d4c6ddb17618faa5f130b66c28de44905
Army <army8735@qq.com>
2025-07-21 10:17:20 +0800

    test: add test

---
+++ test/group4/history-point-flip/test.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+  'init': function(browser) {
+    browser
+      .url('file://' + path.join(__dirname, 'index.html'))
+      .waitForElementVisible('body', 1000)
+      .pause(20)
+      .moveToElement('canvas', 100, 100)
+      .doubleClick()
+      .assert.cssProperty('#main .geometry', 'left', '158.713px')
+      .assert.cssProperty('#main .geometry', 'top', '14.2385')
+      .assert.cssProperty('#main .geometry', 'transform', 'matrix(-0.866025, 0.5, 0.5, 0.866025, 0, 0)')
+      .assert.cssProperty('#main .geometry .vt[title="1"]', 'transform', 'matrix(1, 0, 0, 1, 149.713, 0)')
+
+      .moveToElement('#main .geometry .vt[title="1"]', 0, 0)
+      .mouseButtonDown(0)
+      .assert.cssClassPresent('#main .geometry .vt[title="1"]', ['vt', 'cur'])
+      .moveToElement('#main .geometry .vt[title="1"]', 10, 10)
+      .mouseButtonUp(0)
+      .click('#button6')
+      .assert.value('#base64', '')
+
+      .end();
+  }
+};
