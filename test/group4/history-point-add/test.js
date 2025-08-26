15d5562c08071691b38ab5f3ebf50ba1ec7b1cb2
Army <army8735@qq.com>
2025-08-26 17:28:38 +0800

    test:

--- test/group4/history-point-add/test.js
+++ test/group4/history-point-add/test.js
@@ -32,7 +32,7 @@ module.exports = {
       .assert.cssProperty('#main .geometry .vt.new', 'display', 'none')
       .assert.not.cssClassPresent('#main .canvas-c', 'add-pen')
       .click('#button6')
-      .assert.value('#base64', '')
+      .assert.value('#base64', '[1,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":1,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":0,"tx":1,"ty":0,"absX":150,"absY":0,"absTx":150,"absTy":0,"absFx":150,"absFy":0},{"x":1,"y":0.5,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":0.5,"tx":1,"ty":0.5,"absX":150,"absY":100,"absTx":150,"absTy":100,"absFx":150,"absFy":100},{"x":0,"y":0.5,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0.5,"tx":0,"ty":0.5,"absX":0,"absY":100,"absTx":0,"absTy":100,"absFx":0,"absFy":100},{"x":0,"y":1,"cornerRadius":0,"curveMode":2,"hasCurveFrom":true,"hasCurveTo":true,"fx":-0.6666666666666666,"fy":1,"tx":0.6666666666666666,"ty":1,"absX":0,"absY":200,"absTx":100,"absTy":200,"absFx":-100,"absFy":200}]]')
 
       .keys(browser.Keys.META)
       .keys('z')
@@ -41,6 +41,13 @@ module.exports = {
       .assert.value('#base64', '')
 
       .keys(browser.Keys.META)
+      .keys('z')
+      .keys(browser.Keys.NULL)
+      .click('#button6')
+      .assert.value('#base64', '[3,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":1,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":0,"tx":1,"ty":0,"absX":150,"absY":0,"absTx":150,"absTy":0,"absFx":150,"absFy":0},{"x":1,"y":1,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":1,"tx":1,"ty":1,"absX":150,"absY":100,"absTx":150,"absTy":100,"absFx":150,"absFy":100},{"x":0,"y":1,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":1,"tx":0,"ty":1,"absX":0,"absY":100,"absTx":0,"absTy":100,"absFx":0,"absFy":100}]]')
+
+      .keys(browser.Keys.META)
+      .keys(browser.Keys.SHIFT)
       .keys('z')
       .keys(browser.Keys.NULL)
       .click('#button6')
@@ -51,14 +58,7 @@ module.exports = {
       .keys('z')
       .keys(browser.Keys.NULL)
       .click('#button6')
-      .assert.value('#base64', '')
-
-      .keys(browser.Keys.META)
-      .keys(browser.Keys.SHIFT)
-      .keys('z')
-      .keys(browser.Keys.NULL)
-      .click('#button6')
-      .assert.value('#base64', '')
+      .assert.value('#base64', '[5,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":1,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":0,"tx":1,"ty":0,"absX":150,"absY":0,"absTx":150,"absTy":0,"absFx":150,"absFy":0},{"x":1,"y":1,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":1,"tx":1,"ty":1,"absX":150,"absY":100,"absTx":150,"absTy":100,"absFx":150,"absFy":100},{"x":0,"y":1,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":1,"tx":0,"ty":1,"absX":0,"absY":100,"absTx":0,"absTy":100,"absFx":0,"absFy":100}]]')
 
       .end();
   }
