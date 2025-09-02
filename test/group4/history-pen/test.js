e2b96edaf8037a235ed0376634eb138a290e49d4
Army <army8735@qq.com>
2025-09-02 12:00:12 +0800

    test:

--- test/group4/history-pen/test.js
+++ test/group4/history-pen/test.js
@@ -29,7 +29,7 @@ module.exports = {
       .mouseButtonUp(0)
       .assert.elementPresent('#main .geometry svg path[title="2"]')
       .click('#button6')
-      .assert.value('#base64', '')
+      .assert.value('#base64', '[0,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":1,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":0,"tx":1,"ty":0,"absX":100,"absY":0,"absTx":100,"absTy":0,"absFx":100,"absFy":0},{"x":1,"y":1,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":1,"tx":1,"ty":1,"absX":100,"absY":100,"absTx":100,"absTy":100,"absFx":100,"absFy":100}]]')
       .click('#button2')
       .assert.value('#base64', '')
 
@@ -38,14 +38,14 @@ module.exports = {
       .keys(browser.Keys.NULL)
       .assert.not.elementPresent('#main .geometry svg path[title="2"]')
       .click('#button6')
-      .assert.value('#base64', '')
+      .assert.value('#base64', '[2,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":1,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":0,"tx":1,"ty":0,"absX":100,"absY":0,"absTx":100,"absTy":0,"absFx":100,"absFy":0}]')
 
       .keys(browser.Keys.META)
       .keys('z')
       .keys(browser.Keys.NULL)
       .assert.not.elementPresent('#main .geometry svg path[title="1"]')
       .click('#button6')
-      .assert.value('#base64', '')
+      .assert.value('#base64', '[3,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0}]')
 
       .keys(browser.Keys.META)
       .keys(browser.Keys.SHIFT)
@@ -53,7 +53,7 @@ module.exports = {
       .keys(browser.Keys.NULL)
       .assert.elementPresent('#main .geometry svg path[title="1"]')
       .click('#button6')
-      .assert.value('#base64', '')
+      .assert.value('#base64', '[4,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":1,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":0,"tx":1,"ty":0,"absX":100,"absY":0,"absTx":100,"absTy":0,"absFx":100,"absFy":0}]')
 
       .keys(browser.Keys.META)
       .keys(browser.Keys.SHIFT)
@@ -61,7 +61,7 @@ module.exports = {
       .keys(browser.Keys.NULL)
       .assert.elementPresent('#main .geometry svg path[title="2"]')
       .click('#button6')
-      .assert.value('#base64', '')
+      .assert.value('#base64', '[5,[{"x":0,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":0,"fy":0,"tx":0,"ty":0,"absX":0,"absY":0,"absTx":0,"absTy":0,"absFx":0,"absFy":0},{"x":1,"y":0,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":0,"tx":1,"ty":0,"absX":100,"absY":0,"absTx":100,"absTy":0,"absFx":100,"absFy":0},{"x":1,"y":1,"cornerRadius":0,"curveMode":1,"hasCurveFrom":false,"hasCurveTo":false,"fx":1,"fy":1,"tx":1,"ty":1,"absX":100,"absY":100,"absTx":100,"absTy":100,"absFx":100,"absFy":100}]]')
 
       .end();
   }
